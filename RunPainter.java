2ebfe9f4d2b386f50ad53cf5c3cb917ffa99e81c
secbajor <secbajor@gmail.com>
2013-08-15 14:13:07 -0700

    added 8.15.13

---
+++ RunPainter.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class RunPainter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Painter canvas = new Painter(); 
+		canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
